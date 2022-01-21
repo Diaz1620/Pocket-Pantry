918ba13883843ea1e073a155dc6cc18b539bc2a7
iAmKevinCruz <kcruzroyalty@gmail.com>
2022-01-21 16:36:23 -0500

    Login component validates that email matches the Users in DB. Need to add password validation, then create the createAccount component as a modal to overlap the userLoginPage

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -2,12 +2,42 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 import "./login.css";
 
-const Login = () => {
-  const [users, setUsers] = useState([]);
+const Login = (props) => {
+  const [usersList, setUsersList] = useState([]);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    let guy = { ...user };
+    let list = [...usersList];
+
+    for (let i = 0; i < list.length; i++) {
+      let u = list[i];
+      if (guy.email == u.email) {
+        props.valid(true);
+        console.log("Correct Login");
+      } else {
+        console.log("Wrong Login");
+      }
+    }
+  };
+
+  const test = () => {
+    console.log(user);
+  };
 
   useEffect(() => {
     Axios.get("http://localhost:8000/api/users")
-      .then((res) => setUsers(res.data.results))
+      .then((res) => setUsersList(res.data.results))
       .catch((err) => console.log(err));
   }, []);
 
@@ -18,18 +48,33 @@ const Login = () => {
         <div class="row">
           <div class="input-field col s6 push-s3">
             <i class="material-icons prefix">email</i>
-            <input id="icon_prefix" type="text" class="validate" />
+            <input
+              id="icon_prefix"
+              type="text"
+              name="email"
+              className="validate"
+              onChange={handleInputChange}
+            />
             <label for="icon_prefix">Email</label>
           </div>
         </div>
         <div className="row">
           <div class="input-field col s6 push-s3">
             <i class="material-icons prefix">lock</i>
-            <input id="icon_telephone" type="tel" class="validate" />
+            <input
+              id="icon_telephone"
+              type="text"
+              name="password"
+              className="validate"
+              onChange={handleInputChange}
+            />
             <label for="icon_telephone">Password</label>
           </div>
         </div>
       </form>
+      <button onClick={handleSubmit} className="btn waves-effect">
+        Test
+      </button>
     </div>
   );
 };
--- client/src/components/userLoginPage.jsx
+++ client/src/components/userLoginPage.jsx
@@ -6,13 +6,18 @@ import Login from "./login";
 const UserLoginPage = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
+  const handleLogin = (validation) => {
+    setLoggedIn(validation);
+  };
+
   return loggedIn ? (
     <>
       <h2>You're loggin in!</h2>
+      <p>Insert Profile Page Here</p>
     </>
   ) : (
     <>
-      <Login></Login>
+      <Login valid={handleLogin} />
     </>
   );
 };
