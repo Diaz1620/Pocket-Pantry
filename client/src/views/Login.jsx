c40d01e4760a1b6c46b713c31945786627593828
iAmKevinCruz <kcruzroyalty@gmail.com>
2022-02-03 12:39:35 -0500

    edited icons in register page

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -2,7 +2,7 @@ import "./Login.css";
 import Axios from "axios";
 import { useEffect, useState } from "react";
 // import LoginPage from "../components/loginPage";
-import { navigate } from '@reach/router';
+import { navigate } from "@reach/router";
 
 const Login = () => {
   const [user, setUser] = useState({
@@ -21,15 +21,15 @@ const Login = () => {
     e.preventDefault();
 
     Axios.post("http://localhost:8000/api/login", user)
-        // .then(res => console.log(user))
-        .then(res => navigate("/list"))
-        .catch(err => {
-          console.log(err.response.data.errors);
-        });
+      // .then(res => console.log(user))
+      .then((res) => navigate("/list"))
+      .catch((err) => {
+        console.log(err.response.data.errors);
+      });
   };
 
   return (
-    <div class="row">
+    <div class="row login">
       <h1>Login</h1>
       <form onSubmit={handleSubmit} class="col s12 container">
         <div class="row">
@@ -58,39 +58,38 @@ const Login = () => {
             <label for="icon_telephone">Password</label>
           </div>
         </div>
-        <input type="submit" value="Login" className="btn waves-effect" />
+        <input
+          type="submit"
+          value="Login"
+          className="btn waves-effect loginBtn"
+        />
       </form>
+      <a className="registerLink" href="/register">
+        Not a member? Click here to register!
+      </a>
       {/* <button type="submit" className="btn waves-effect">
         Test
       </button> */}
     </div>
   );
-
 };
 
 export default Login;
-  
-  
-  
-  
-  
-  
-  
-  
-  //   const [loggedIn, setLoggedIn] = useState(false);
-  
-  //   const handleLogin = (validation) => {
-  //     setLoggedIn(validation);
-  //   };
-  
-  //   return loggedIn ? (
-  //     <>
-  //       <h2>You're loggin in!</h2>
-  //       <p>Insert Profile Page Here</p>
-  //     </>
-  //   ) : (
-  //     <>
-  //       <LoginPage/>
-  //     </>
-  //   );
-  // };
+
+//   const [loggedIn, setLoggedIn] = useState(false);
+
+//   const handleLogin = (validation) => {
+//     setLoggedIn(validation);
+//   };
+
+//   return loggedIn ? (
+//     <>
+//       <h2>You're loggin in!</h2>
+//       <p>Insert Profile Page Here</p>
+//     </>
+//   ) : (
+//     <>
+//       <LoginPage/>
+//     </>
+//   );
+// };
--- client/src/views/UserCreate.jsx
+++ client/src/views/UserCreate.jsx
@@ -3,116 +3,116 @@ import { Axios } from "axios";
 import { navigate } from "@reach/router";
 
 const UserCreate = () => {
-    const [user, setUser] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleInputChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const [errors, setErrors] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-    });
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleInputChange = (e) => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value,
-        })
-    };
+    Axios.post("http://localhost:8000/api/register", user)
+      .then((res) => navigate("/list"))
+      .catch((err) => {
+        console.log(err.response.data.errors);
+        setErrors(err.response.data.errors);
+      });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        Axios.post("http://localhost:8000/api/register", user)
-        .then((res) => navigate("/list"))
-        .catch((err) => {
-            console.log(err.response.data.errors);
-            setErrors(err.response.data.errors);
-        });
-    };
-
-    return (
+  return (
+    <div class="row">
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit} class="col s12 container">
+        <div className="row">
+          <div class="input-field col s6 push-s3">
+            <i class="material-icons prefix">account_circle</i>
+            <input
+              id="icon_telephone"
+              type="text"
+              name="firstName"
+              className="validate"
+              onChange={handleInputChange}
+            />
+            <label for="icon_telephone">First Name:</label>
+          </div>
+        </div>
         <div class="row">
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit} class="col s12 container">
-            <div className="row">
-                <div class="input-field col s6 push-s3">
-                    <i class="material-icons prefix">lock</i>
-                    <input
-                    id="icon_telephone"
-                    type="text"
-                    name="firstName"
-                    className="validate"
-                    onChange={handleInputChange}
-                    />
-                    <label for="icon_telephone">First Name:</label>
-                </div>
-                </div>
-                <div class="row">
-                <div class="input-field col s6 push-s3">
-                    <i class="material-icons prefix">email</i>
-                    <input
-                    id="icon_prefix"
-                    type="text"
-                    name="lastName"
-                    className="validate"
-                    onChange={handleInputChange}
-                    />
-                    <label for="icon_prefix">Last Name:</label>
-                </div>
-                </div>
-                <div class="row">
-                <div class="input-field col s6 push-s3">
-                    <i class="material-icons prefix">email</i>
-                    <input
-                    id="icon_prefix"
-                    type="text"
-                    name="email"
-                    className="validate"
-                    onChange={handleInputChange}
-                    />
-                    <label for="icon_prefix">Email</label>
-                </div>
-                </div>
-                <div className="row">
-                <div class="input-field col s6 push-s3">
-                    <i class="material-icons prefix">lock</i>
-                    <input
-                    id="icon_telephone"
-                    type="text"
-                    name="password"
-                    className="validate"
-                    onChange={handleInputChange}
-                    />
-                    <label for="icon_telephone">Password</label>
-                </div>
-                </div>
-                <div className="row">
-                <div class="input-field col s6 push-s3">
-                    <i class="material-icons prefix">lock</i>
-                    <input
-                    id="icon_telephone"
-                    type="text"
-                    name="confirmPassword"
-                    className="validate"
-                    onChange={handleInputChange}
-                    />
-                    <label for="icon_telephone">Confirm Password</label>
-                </div>
-                </div>
-                <input type="submit" value="Login" className="btn waves-effect" />
-            </form>
-            {/* <button type="submit" className="btn waves-effect">
+          <div class="input-field col s6 push-s3">
+            <i class="material-icons prefix">account_circle</i>
+            <input
+              id="icon_prefix"
+              type="text"
+              name="lastName"
+              className="validate"
+              onChange={handleInputChange}
+            />
+            <label for="icon_prefix">Last Name:</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6 push-s3">
+            <i class="material-icons prefix">email</i>
+            <input
+              id="icon_prefix"
+              type="text"
+              name="email"
+              className="validate"
+              onChange={handleInputChange}
+            />
+            <label for="icon_prefix">Email</label>
+          </div>
+        </div>
+        <div className="row">
+          <div class="input-field col s6 push-s3">
+            <i class="material-icons prefix">lock</i>
+            <input
+              id="icon_telephone"
+              type="text"
+              name="password"
+              className="validate"
+              onChange={handleInputChange}
+            />
+            <label for="icon_telephone">Password</label>
+          </div>
+        </div>
+        <div className="row">
+          <div class="input-field col s6 push-s3">
+            <i class="material-icons prefix">lock</i>
+            <input
+              id="icon_telephone"
+              type="text"
+              name="confirmPassword"
+              className="validate"
+              onChange={handleInputChange}
+            />
+            <label for="icon_telephone">Confirm Password</label>
+          </div>
+        </div>
+        <input type="submit" value="Login" className="btn waves-effect" />
+      </form>
+      {/* <button type="submit" className="btn waves-effect">
                 Test
             </button> */}
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default UserCreate;
